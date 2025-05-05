85acc65049368e92504d58c5ddb2bdaa1e1be778
AdrianSep1 <adriansepulveda2917@gmail.com>
2025-05-06 00:36:47 +0800

    Delete KMC Initial Network Checking Tool v1.2/KMC Initial Network Checking Tool v1.2 directory

--- KMC Initial Network Checking Tool v1.2/KMC Initial Network Checking Tool v1.2/KMC Initial Network Checking Tool.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting KMC Network Checking Tool...
-echo.
-
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0KMC Initial Network Checking Tool.ps1"
-
-echo.
-echo Press any key to exit...
-pause > nul 
